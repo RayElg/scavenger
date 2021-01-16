300985603ffb4cc308edf1bb94f6cfbc22b964e8
Raynor Elgie <elgieraynor@gmail.com>
2021-01-16 00:15:57 -0500

    placeholder generation & view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dTypes.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,14 +15,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Scavenger: Games'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
+  List<Game> games = [];
   final String title;
 
   @override
@@ -40,6 +41,13 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+    Tag a = new Tag("Chapeau", "hat");
+    Game g =
+        new Game("Scavenger Hunt!", [a.id], "Its a scavenger hunt, of course!");
+    User u = new User("raynor");
+    a.gameId = g.id;
+    print(g.id);
+    widget.games.add(g);
   }
 
   @override
@@ -50,6 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -60,29 +69,29 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            SizedBox(width: 1, height: 8),
+            Text("Active games:"),
+            Expanded(
+              child: ListView(
+                children: [
+                  for (Game g in widget.games)
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                          padding: EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                              border: Border(),
+                              color: Color.fromARGB(100, 255, 50, 255)),
+                          child: Column(children: [
+                            Row(
+                              children: [Text(g.title)],
+                            ),
+                          ])),
+                    ),
+                ],
+              ),
+            )
           ],
         ),
       ),
