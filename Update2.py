969e5b529679922f664177a19073f976308c8914
Asy <60577788+System-out-print-Asgar@users.noreply.github.com>
2021-01-16 19:11:14 -0500

    Add files via upload

---
+++ Update2.py
@@ -0,0 +1,14 @@
+#imports 
+
+import time
+import random
+import logging
+
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+
+import psycopg2
+from psycopg2.errors import SerializationFailure
+
+
+
