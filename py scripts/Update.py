3c9d275bc11de2bec89247a30fa03592692be62a
Asy <60577788+System-out-print-Asgar@users.noreply.github.com>
2021-01-17 20:19:28 -0500

    Update Update.py

--- py scripts/Update.py
+++ py scripts/Update.py
@@ -1,6 +1,9 @@
+#imports
+
 import time
 import random
 import logging
+import sys
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
@@ -9,169 +12,163 @@ import psycopg2
 from psycopg2.errors import SerializationFailure
 
 
-def create_accounts(conn):
+
+
+
+#NAME-ID
+#METHOD TO CREATE NEW USER IN DB
+def create_new_user(conn, ID, NAME):
+
     with conn.cursor() as cur:
         cur.execute(
-            "CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)"
+            "INSERT INTO Players (ID, Name) VALUES ('%s', '%s')" % (ID,NAME)
         )
-        cur.execute("UPSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)")
+
+        logging.debug("create_accounts(): status message: %s", cur.statusmessage)
+    conn.commit()
+
+#METHOD TO UPDATE USER IN DB
+def update_user(conn, ID, NAME):
+    with conn.cursor() as cur:
+        cur.execute(
+            "UPDATE Players SET Name ='%s' WHERE ID='%s'" % (NAME, ID)
+        )
+
         logging.debug("create_accounts(): status message: %s", cur.statusmessage)
     conn.commit()
 
 
-def delete_accounts(conn):
+
+
+#TITLE-TAGS-DESCRIPTION-ID-HOST
+#METHOD TO CREATE NEW GAME IN DB
+def create_new_Game(conn, ID, title, description, tags, host):
     with conn.cursor() as cur:
-        cur.execute("DELETE FROM defaultdb.accounts")
-        logging.debug("delete_accounts(): status message: %s", cur.statusmessage)
+        cur.execute(
+            "INSERT INTO Games (ID, title, description, tags, host) VALUES ('%s', '%s', '%s', '%s', '%s')" % (ID, title, description, tags, host)
+        )
+
+        logging.debug("create_accounts(): status message: %s", cur.statusmessage)
+    conn.commit()
+
+#METHOD TO UPDATE GAME IN DB
+def update_Game(conn, ID, title, description, tags, host):
+    with conn.cursor() as cur:
+        cur.execute(
+            "UPDATE Games SET title='%s', description='%s', tags='%s', host='%s' WHERE ID='%s'" % (title, description, tags, host, ID)
+        )
+
+        logging.debug("create_accounts(): status message: %s", cur.statusmessage)
     conn.commit()
 
 
-def print_balances(conn):
+
+
+
+#TITLE-TAG-HASSCORED-VALUE-ID
+#METHOD TO CREATE NEW TAG IN DB
+def create_new_tag(conn, ID, title, tag, value, hasScored):
+    value = int(value)
     with conn.cursor() as cur:
-        cur.execute("SELECT id, balance FROM accounts")
-        logging.debug("print_balances(): status message: %s", cur.statusmessage)
-        rows = cur.fetchall()
-        conn.commit()
-        print(f"Balances at {time.asctime()}:")
-        for row in rows:
-            print(row)
-
-
-def transfer_funds(conn, frm, to, amount):
-    with conn.cursor() as cur:
-
-        # Check the current balance.
-        cur.execute("SELECT balance FROM accounts WHERE id = %s", (frm,))
-        from_balance = cur.fetchone()[0]
-        if from_balance < amount:
-            raise RuntimeError(
-                f"Insufficient funds in {frm}: have {from_balance}, need {amount}"
-            )
-
-        # Perform the transfer.
         cur.execute(
-            "UPDATE accounts SET balance = balance - %s WHERE id = %s", (amount, frm)
-        )
-        cur.execute(
-            "UPDATE accounts SET balance = balance + %s WHERE id = %s", (amount, to)
+            "INSERT INTO Tags (ID, title, tag, value, hasScored) VALUES ('%s', '%s', '%s', %d, '%s')" % (ID, title, tag, value, hasScored)
         )
 
+        logging.debug("create_accounts(): status message: %s", cur.statusmessage)
     conn.commit()
-    logging.debug("transfer_funds(): status message: %s", cur.statusmessage)
 
 
-def run_transaction(conn, op, max_retries=3):
-    """
-    Execute the operation *op(conn)* retrying serialization failure.
 
-    If the database returns an error asking to retry the transaction, retry it
-    *max_retries* times before giving up (and propagate it).
-    """
-    # leaving this block the transaction will commit or rollback
-    # (if leaving with an exception)
-    with conn:
-        for retry in range(1, max_retries + 1):
-            try:
-                op(conn)
-
-                # If we reach this point, we were able to commit, so we break
-                # from the retry loop.
-                return
-
-            except SerializationFailure as e:
-                # This is a retry error, so we roll back the current
-                # transaction and sleep for a bit before retrying. The
-                # sleep time increases for each failed transaction.
-                logging.debug("got error: %s", e)
-                conn.rollback()
-                logging.debug("EXECUTE SERIALIZATION_FAILURE BRANCH")
-                sleep_ms = (2 ** retry) * 0.1 * (random.random() + 0.5)
-                logging.debug("Sleeping %s seconds", sleep_ms)
-                time.sleep(sleep_ms)
-
-            except psycopg2.Error as e:
-                logging.debug("got error: %s", e)
-                logging.debug("EXECUTE NON-SERIALIZATION_FAILURE BRANCH")
-                raise e
-
-        raise ValueError(f"Transaction did not succeed after {max_retries} retries")
-
-
-def test_retry_loop(conn):
-    """
-    Cause a seralization error in the connection.
-
-    This function can be used to test retry logic.
-    """
+#METHOD TO UPDATE TAG IN DB
+def update_tag(conn, ID, title, tag, value, hasScored):
+    value = int(value)
     with conn.cursor() as cur:
-        # The first statement in a transaction can be retried transparently on
-        # the server, so we need to add a dummy statement so that our
-        # force_retry() statement isn't the first one.
-        cur.execute("SELECT now()")
-        cur.execute("SELECT crdb_internal.force_retry('1s'::INTERVAL)")
-    logging.debug("test_retry_loop(): status message: %s", cur.statusmessage)
+        cur.execute(
+            "UPDATE Tags SET title='%s', tag='%s', value=%d, hasScored='%s' WHERE ID='%s'" % (title, tag, value, hasScored, ID)
+        )
 
+        logging.debug("create_accounts(): status message: %s", cur.statusmessage)
+    conn.commit()
 
-def main():
-    opt = parse_cmdline()
-    logging.basicConfig(level=logging.DEBUG if opt.verbose else logging.INFO)
-
-    conn = psycopg2.connect(opt.dsn)
-    create_accounts(conn)
-    print_balances(conn)
-
-    amount = 100
-    fromId = 1
-    toId = 2
-
-    try:
-        run_transaction(conn, lambda conn: transfer_funds(conn, fromId, toId, amount))
-
-        # The function below is used to test the transaction retry logic.  It
-        # can be deleted from production code.
-        # run_transaction(conn, test_retry_loop)
-    except ValueError as ve:
-        # Below, we print the error and continue on so this example is easy to
-        # run (and run, and run...).  In real code you should handle this error
-        # and any others thrown by the database interaction.
-        logging.debug("run_transaction(conn, op) failed: %s", ve)
-        pass
-
-    print_balances(conn)
-
-    delete_accounts(conn)
-
-    # Close communication with the database.
-    conn.close()
-
-
-def parse_cmdline():
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=RawTextHelpFormatter)
-    parser.add_argument(
-        "dsn",
-        help="database connection string\n\n"
-             "For cockroach demo, use postgresql://<username>:<password>@<hostname>:<port>/bank?sslmode=require,\n"
-             "with the username and password created in the demo cluster, and the hostname and port listed in the\n"
-             "(sql/tcp) connection parameters of the demo cluster welcome message."
-    )
-
-    parser.add_argument("-v", "--verbose",
-                        action="store_true", help="print debug info")
-
-    opt = parser.parse_args()
-    return opt
 
 
 if __name__ == "__main__":
-    main()
 
 
-#'postgresql://<username>:<password>@<hostname>:<port>/bank?sslmode=require'
-#'postgresql://<bilal>:<JINXMAIN123A>@<free-tier.gcp-us-central1.cockroachlabs.cloud>:<26257>/bank?sslmode=require'
+    conn = psycopg2.connect(
+    user='root',
+    database='defaultdb',
+    port=26257,
+    host='localhost')
 
 
-#'cockroach sql --url 'postgres://bilal:JINXMAIN123A@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/vital-sheep-224.defaultdb?sslmode=verify-full&sslrootcert=C:\Users\Asy\Desktop\roachDB/cc-ca.crt'
+#players table
+#runs when first arg=='Players'
+    if (sys.argv[1]=='Players'):
 
-#password no longer JINXMAIN123A 
-#new passoword BASEDBOYSHackTheNorth
+        Param1=sys.argv[2]
+        Param2=sys.argv[3]
+
+        create_new_user(conn, Param1, Param2)
+
+#update players table
+#runs when first arg=='PlayersUpdate'
+    if (sys.argv[1]=='PlayersUpdate'):
+
+        Param1=sys.argv[2]
+        Param2=sys.argv[3]
+        Param3=sys.argv[4]
+
+        update_user(conn, Param1, Param2, Param3)
+
+
+#tags table
+#runs when first arg=='Tags'
+    if (sys.argv[1]=='Tags'):
+
+        Param1=sys.argv[2]
+        Param2=sys.argv[3]
+        Param3=sys.argv[4]
+        Param4=sys.argv[5]
+        Param5=sys.argv[6]
+
+        create_new_tag(conn, Param1, Param2, Param3, Param4, Param5)
+
+#update Tags table
+#runs when first arg=='TagsUpdate'
+    if (sys.argv[1]=='TagsUpdate'):
+
+        Param1=sys.argv[2]
+        Param2=sys.argv[3]
+        Param3=sys.argv[4]
+        Param4=sys.argv[5]
+        Param5=sys.argv[6]
+
+        update_tag(conn, Param1, Param2, Param3, Param4, Param5)
+
+
+
+
+#game table
+#runs when first arg=='Game'
+    if (sys.argv[1]=='Game'):
+
+        Param1=sys.argv[2]
+        Param2=sys.argv[3]
+        Param3=sys.argv[4]
+        Param4=sys.argv[5]
+        Param5=sys.argv[6]
+
+        create_new_Game(conn, Param1, Param2, Param3, Param4, Param5)
+
+#update games table
+#runs when first arg=='GameUpdate'
+    if (sys.argv[1]=='GameUpdate'):
+
+        Param1=sys.argv[2]
+        Param2=sys.argv[3]
+        Param3=sys.argv[4]
+        Param4=sys.argv[5]
+        Param5=sys.argv[6]
+
+        update_Game(conn, Param1, Param2, Param3, Param4, Param5)
